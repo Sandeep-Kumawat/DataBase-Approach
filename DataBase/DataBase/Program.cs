2bbf59e14a8f38b3d93613adb98b7d3a64fe18fe
Sandeep-Kumawat <sandeepkumawatsa77@gmail.com>
2021-04-30 14:49:09 +0530

    Database First Approach

---
+++ DataBase/DataBase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           DB1stApproachEntities context = new DB1stApproachEntities();
+            
+            var pro = context.Product_Detail.Include("Products");
+            pro.ToList().ForEach(
+                (i) => {
+                    Console.WriteLine(i.Owner);
+                }
+                );
+        }
+    }
+}
